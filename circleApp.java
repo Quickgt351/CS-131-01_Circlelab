85c0b7806cc193388f82b240e5fee64c6c02c994
Quickgt351 <77062409+Quickgt351@users.noreply.github.com>
2021-01-20 22:06:00 -0500

    Add files via upload

---
+++ circleApp.java
@@ -0,0 +1,12 @@
+
+public class circleApp {
+
+	public static void main(String[] args) {
+		
+		Circle myCircle=new Circle();
+		double area=myCircle.calculateArea(1);
+		
+		System.out.println(area);	
+	}
+
+}
